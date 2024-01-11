eed57eaf490c082871f2073032bfb22bdeef2f95
Maryna <133129896+Maryna1996@users.noreply.github.com>
2024-01-11 19:41:05 +0200

    offer

---
+++ src/main/java/com/example/googleapi/repository/TrendDataRepository.java
@@ -0,0 +1,8 @@
+package com.example.googleapi.repository;
+
+import com.example.googleapi.model.TrendData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrendDataRepository extends JpaRepository<TrendData, Long> {
+
+}
